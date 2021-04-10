b44a648ef2e2b5b88dc22ea4d1247700f02e5f09
Thiago Santos <tos.oliveira@gmail.com>
2021-04-10 17:51:27 -0300

    adding iterable item types

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,7 @@ export type ValueType = string | boolean | number;
 export type Primitive = ValueType | null | undefined;
 export type Sizeable = { size: number };
 export type Lenghtable = { length: number };
+export type IterableItem<T> = T extends Iterable<infer R> ? R : never;
+export type AnyIterable<T> = Iterable<T> | AsyncIterable<T>;
+export type AsyncIterableItem<T> = T extends AsyncIterable<infer R> ? R : never;
+export type AnyIterableItem<T> = T extends AnyIterable<infer R> ? R : never;
