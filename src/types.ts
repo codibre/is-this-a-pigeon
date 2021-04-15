71ac34b67b35a975fd4bc65ececce3c82823deae
Thiago Santos <tos.oliveira@gmail.com>
2021-04-14 21:28:37 -0300

    adding KeysOfType and ClassMethod

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,10 @@ export type AnyIterable<T> = Iterable<T> | AsyncIterable<T>;
 export type AsyncIterableItem<T> = T extends AsyncIterable<infer R> ? R : never;
 export type AnyIterableItem<T> = T extends AnyIterable<infer R> ? R : never;
 export type AbstractConstructorParameters<T extends abstract new (...args: any) => any> = T extends abstract new (...args: infer P) => any ? P : never;
+export type KeysOfType<T, V> = {
+	[K in keyof T]-?: T[K] extends V ? K : never;
+}[keyof T];
+export type ClassMethod<
+T extends Object,
+K extends KeysOfType<T, Func>
+> = T[K];
