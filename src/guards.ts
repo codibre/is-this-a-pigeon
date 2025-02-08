f028fec259c70b24bcbec6327b80ec826bdef3c4
Thiago Santos <tos.oliveira@gmail.com>
2025-02-08 08:17:34 -0300

    feat: isFulfilled and isRejected

--- src/guards.ts
+++ src/guards.ts
@@ -20,6 +20,14 @@ export function isIterable(value: any): value is Iterable<any> {
 	return value && typeof value[Symbol.iterator] === 'function';
 }
 
+export function isFulfilled<T>(value: PromiseSettledResult<T>): value is PromiseFulfilledResult<T> {
+  return value.status === 'fulfilled';
+}
+
+export function isRejected<T>(value: PromiseSettledResult<T>): value is PromiseRejectedResult {
+  return value.status === 'fulfilled';
+}
+
 export function isOnlyIterable(value: any): value is Iterable<any> {
 	return (
 		value &&
