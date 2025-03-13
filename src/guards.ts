24ec42da5beea314603db4b562ad61a2489b0082
Thiago Santos <tos.oliveira@gmail.com>
2025-03-13 08:48:24 -0300

    fix: adding isProperty validation

--- src/guards.ts
+++ src/guards.ts
@@ -20,12 +20,16 @@ export function isIterable(value: any): value is Iterable<any> {
 	return value && typeof value[Symbol.iterator] === 'function';
 }
 
-export function isFulfilled<T>(value: PromiseSettledResult<T>): value is PromiseFulfilledResult<T> {
-  return value.status === 'fulfilled';
+export function isFulfilled<T>(
+	value: PromiseSettledResult<T>,
+): value is PromiseFulfilledResult<T> {
+	return value.status === 'fulfilled';
 }
 
-export function isRejected<T>(value: PromiseSettledResult<T>): value is PromiseRejectedResult {
-  return value.status === 'fulfilled';
+export function isRejected<T>(
+	value: PromiseSettledResult<T>,
+): value is PromiseRejectedResult {
+	return value.status === 'fulfilled';
 }
 
 export function isOnlyIterable(value: any): value is Iterable<any> {
@@ -52,6 +56,14 @@ export function isAnyIterable(value: any): value is AnyIterable<any> {
 
 export function isString(t: any): t is string {
 	return typeof t === 'string';
+}
+
+export function isProperty<K extends string | number | symbol, T>(
+	t: any,
+	prop: K,
+	validation: (p: any) => p is T,
+): t is { [k in K]: T } {
+	return prop in t && validation(t[prop]);
 }
 
 export function isNumber(t: any): t is number {
